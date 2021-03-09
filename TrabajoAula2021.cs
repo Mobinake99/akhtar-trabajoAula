9e53e5ca18c071baf1a7354bb53fad8224cadce2
meark <Mobinenrique@hotmail.com>
2021-03-09 20:07:36 -0300

    segunda prueba, con un cw mas

--- TrabajoAula2021.cs
+++ TrabajoAula2021.cs
@@ -5,6 +5,7 @@ public class Class1
 	public Class1()
 	{
         Console.WriteLine("Hola git");
-		//no me permite crear el archivo WFA
+        //no me permite crear el archivo WFA
+        Console.WriteLine("ayuda, esto es una prueba");
 	}
 }
